2e904a866863f7bc3808be73ac0b8b641c9c90cf
Bosch Leith <bosch.leith@gmail.com>
2021-08-16 13:25:25 -0500

    created: conditionals-lec.html & conditionals-lec.js

---
+++ js/conditionals-lec.js
@@ -0,0 +1,21 @@
+//Sunny Weather IF/ELSE
+var isSunny = false;
+
+if (isSunny) {
+    alert("Don't forget your sunscreen!");
+} else {
+    alert("There is no need for sunscreen.");
+}
+
+//Traffic Light IF/ELSE
+var light = "red"
+
+if (light === "green") {
+    alert("GO!")
+} else if (light === "yellow") {
+    alert("SLOW DOWN!")
+} else if (light === "red") {
+    alert("STOP!")
+} else {
+    alert("LIGHT INOPERATIVE")
+}
