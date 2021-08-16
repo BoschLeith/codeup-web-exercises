3e836007bcb442a0122ac9ae5b8c40444deb15f3
Bosch Leith <bosch.leith@gmail.com>
2021-08-16 16:56:24 -0500

    modified: refactored

--- js/conditionals-lec.js
+++ js/conditionals-lec.js
@@ -1,5 +1,7 @@
 //Sunny Weather IF/ELSE
-var isSunny = false;
+
+var usersWeather = confirm("Is it sunny outside?")
+var isSunny = usersWeather;
 
 if (isSunny) {
     alert("Don't forget your sunscreen!");
@@ -8,14 +10,54 @@ if (isSunny) {
 }
 
 //Traffic Light IF/ELSE
-var light = "red"
+
+var usersLight = prompt("What color is your traffic light?")
+var light = usersLight.toLowerCase();
 
 if (light === "green") {
-    alert("GO!")
+    alert("GO!");
 } else if (light === "yellow") {
-    alert("SLOW DOWN!")
+    alert("SLOW DOWN!");
 } else if (light === "red") {
-    alert("STOP!")
+    alert("STOP!");
 } else {
-    alert("LIGHT INOPERATIVE")
+    alert("LIGHT INOPERATIVE");
+}
+
+//Sunny Weather IF/ELSE before TERNARY
+
+var usersWeather = confirm("Is it sunny outside?")
+var isSunny = usersWeather;
+
+if (isSunny) {
+    alert("It's sunny outside.");
+} else {
+    alert("It's not sunny outside.");
+}
+
+//Sunny Weather after TERNARY
+
+var usersWeather = confirm("Is it sunny outside?")
+var isSunny = usersWeather;
+
+(isSunny) ? alert("It's sunny outside.") : alert("It's not sunny outside.");
+
+//Traffic Light refactored as a SWITCH statement
+
+var usersLight = prompt("What color is your traffic light?")
+var light = usersLight.toLowerCase();
+
+switch (light) {
+    case "green":
+        alert("GO!");
+        break;
+    case "yellow":
+        alert("SLOW DOWN!");
+        break;
+    case "red":
+        alert("STOP!");
+        break;
+    default:
+        alert("LIGHT INOPERATIVE");
+        break;
 }
