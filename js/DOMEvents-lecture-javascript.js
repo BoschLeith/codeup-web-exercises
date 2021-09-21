146a28af665bc1d3d7ed02d12f89afaa5c034a04
Bosch Leith <bosch.leith@gmail.com>
2021-09-21 12:05:19 -0500

    feat: added second event lister and event remover

--- js/DOMEvents-lecture-javascript.js
+++ js/DOMEvents-lecture-javascript.js
@@ -1,12 +1,19 @@
 var button1 = document.getElementById("btn1");
 var button2 = document.getElementById("btn2");
 
-button1.addEventListener("click", function (){
-	// alert("Hello Quasar!");
+var purple = function (){
 
 	var listItems = document.getElementsByTagName("li");
 
 	for (var i = 0; i < listItems.length; i++) {
 		listItems[i].style.backgroundColor = "purple";
 	}
+}
+
+button1.addEventListener("click", purple)
+
+button2.addEventListener("click", function() {
+
+	button1.removeEventListener("click", purple)
+
 })
