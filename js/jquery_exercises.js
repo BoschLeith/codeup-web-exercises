a069c20bd2b26528877c7a86b72c497d77de76f8
Bosch Leith <bosch.leith@gmail.com>
2021-09-28 12:23:12 -0500

    feat: added element selectors and multiple selector

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -7,4 +7,15 @@
 // var content = $("#item1").html();
 // alert(content);
 
-$(".codeup").css("border", "1px solid red");
+// $(".codeup").css("border", "1px solid red");
+
+$("li").css("font-size", "20px");
+
+// $("h1").css("background-color", "yellow");
+// $("p").css("background-color", "yellow");
+// $("li").css("background-color", "yellow");
+
+$("h1, p, li").css("background-color", "yellow");
+
+var content = $("h1").html();
+alert(content);
