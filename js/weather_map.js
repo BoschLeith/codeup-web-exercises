1469a1fa0f47a6f5f50d006ca6ae9a68b2ec62dd
Bosch Leith <bosch.leith@gmail.com>
2021-10-08 16:44:46 -0500

    Submit event now grabs the usersSearch coordinates and place_name. Updates the map to the usersSearch.

--- js/weather_map.js
+++ js/weather_map.js
@@ -10,57 +10,55 @@ function convertDateTime(unixTime) {
 	return dateObject.toLocaleString("en-US", options)
 }
 
-function currentCard(data){
-	var iconCode = data.current.weather[0].icon;
-	var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-
+function renderCards(data, mapData){
 	var html = "";
-	html += "<div class='card mb-3 p-0'>" +
-		"<div class='row'>" +
-		"<div>" +
-		"<div class='card-header text-center'>" + "Today's Weather" + "</div>" +
-		"<div class='card-body text-center'>" +
-		"<h5 class='h-5 card-title'>"+ "San Antonio" + "</h5>" +
-		"<div>" + "<img id='weeklyWeatherIcon' class='img-fluid' src=" + iconUrl + " alt='Weather icon'>" + "</div>" +
-		"<h1 class='h-1 card-text'>" + data.current.temp.toFixed(0) + "<span>" + "&#8457;" + "</span>" + "</h1>" +
-		"<div>" + "<span>" + "H:" + "</span>" + data.daily[0].temp.max.toFixed(0) + " " + "<span>" + "L:" + "</span>" + data.daily[0].temp.min.toFixed(0) + "</div>" +
-		"<div>" + "You've got some " + data.current.weather[0].description.toLowerCase() + " going on." + "</div>" +
-		"</div>" +
-		"</div>" +
-		"</div>" +
-		"</div>"
-	return html
-}
+	for (var i = 0; i <= 4; i++){
+		if (i === 0){
+			html += "<div class='card mb-3 p-0'>" +
+				"<div class='row'>" +
+				"<div>" +
+				"<div class='card-header text-center bg-secondary text-white'>" + "Today's Weather" + "</div>" +
+				"<div class='card-body text-center'>" +
+				"<h5 class='h-5 card-title'>"+ mapData.features[0].place_name + "</h5>" +
+				"<h1 class='h-1 card-text'>" + "<img id='weeklyWeatherIcon' class='img-fluid' src=" + "https://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png" + " alt='Weather icon'>" + "</h1>" +
+				"<h1 class='h-1 card-text'>" + data.current.temp.toFixed(0) + "<span>" + "&#8457;" + "</span>" + "</h1>" +
+				"<div>" + "<span>" + "H:" + "</span>" + data.daily[i].temp.max.toFixed(0) + " " + "<span>" + "L:" + "</span>" + data.daily[i].temp.min.toFixed(0) + "</div>" +
+				"<div>" + "You've got some " + data.daily[i].weather[0].description.toLowerCase() + " going on." + "</div>" +
+				"</div>" +
+				"</div>" +
+				"</div>" +
+				"</div>"
+			todayCard.html(html)
+			html = "";
+		} else {
 
-function renderWeeklyCards(data){
-	var html = "";
-	for (var i = 1; i < 5; i++){
-		html += "<div class='card mb-3 p-0'>" +
-			"<div>" +
-			"<div>" +
-			"<div class='card-header text-center'>" + convertDateTime(data.daily[i].dt) + "</div>" +
-			"<div class='card-body text-center'>" +
-			"<h1 class='h-1 card-text'>" + "<img id='weeklyWeatherIcon' class='img-fluid' src=" + "https://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png" + " alt='Weather icon'>" + "</h1>" +
-			"<div>" + "<span>" + "H:" + "</span>" + data.daily[i].temp.max.toFixed(0) + " " + "<span>" + "L:" + "</span>" + data.daily[i].temp.min.toFixed(0) + "</div>" +
-			"<div>" + data.daily[i].weather[0].main + "</div>" +
-			"</div>" +
-			"</div>" +
-			"</div>" +
-			"</div>"
+			html += "<div class='card mb-3 p-0'>" +
+				"<div>" +
+				"<div>" +
+				"<div class='card-header text-center bg-secondary text-white'>" + convertDateTime(data.daily[i].dt) + "</div>" +
+				"<div class='card-body text-center'>" +
+				"<h1 class='h-1 card-text'>" + "<img id='weeklyWeatherIcon' class='img-fluid' src=" + "https://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png" + " alt='Weather icon'>" + "</h1>" +
+				"<div>" + "<span>" + "H:" + "</span>" + data.daily[i].temp.max.toFixed(0) + " " + "<span>" + "L:" + "</span>" + data.daily[i].temp.min.toFixed(0) + "</div>" +
+				"<div>" + data.daily[i].weather[0].main + "</div>" +
+				"</div>" +
+				"</div>" +
+				"</div>" +
+				"</div>"
+			weeklyCards.html(html)
+		}
+
 	}
-	return html
 }
-
-$.get("https://api.openweathermap.org/data/2.5/onecall", {
-	appid: openWeatherKey,
-	lat: 29.4246,
-	lon: -98.4951,
-	units: "imperial"
-}).done(function(data) {
-	console.log(data);
-	todayCard.html(currentCard(data));
-	weeklyCards.html(renderWeeklyCards(data));
-});
+geocode("San Antonio, TX", mapBoxKey).then(function (mapData){
+	$.get("https://api.openweathermap.org/data/2.5/onecall", {
+		appid: openWeatherKey,
+		lat: 29.4246,
+		lon: -98.4951,
+		units: "imperial"
+	}).done(function(data) {
+		renderCards(data, mapData);
+	});
+})
 
 mapboxgl.accessToken = mapBoxKey;
 
@@ -68,7 +66,7 @@ var map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/streets-v11',
 	zoom: 10,
-	center: [-98.4683, 29.5312]
+	center: [-98.4951, 29.4246]
 });
 
 var usersCoordinates = [];
@@ -79,19 +77,16 @@ $("#submit").click(function (e){
 	usersMapMarker(usersSearch, mapBoxKey);
 	(geocode(usersSearch, mapBoxKey)).then(function (mapData){
 		console.log(mapData);
-		console.log(mapData.features[3].center);
-		console.log(mapData.features[3].place_name)
 		usersCoordinates = mapData
-		// console.log(usersCoordinates);
+		console.log(usersCoordinates);
 		$.get("https://api.openweathermap.org/data/2.5/onecall", {
 			appid: openWeatherKey,
-			lat: usersCoordinates.features[3].center[1],
-			lon: usersCoordinates.features[3].center[0],
+			lat: usersCoordinates.features[0].center[1],
+			lon: usersCoordinates.features[0].center[0],
 			units: "imperial"
 		}).done(function(data) {
 			console.log(data);
-			todayCard.html(currentCard(data));
-			weeklyCards.html(renderWeeklyCards(data));
+			renderCards(data, mapData);
 		});
 	})
 })
@@ -100,21 +95,12 @@ function usersMapMarker(search, token){
 	geocode(search, token).then(function(results){
 
 		map.flyTo({
-			center: results.features[3].center,
+			center: results.features[0].center,
 			zoom: 15
 		});
 
-
-		var popUp = new mapboxgl.Popup()
-			.setHTML("<div>" +
-				"<div>" + search + "</div>" +
-				"</div>")
-
 		new mapboxgl.Marker()
-			.setLngLat(results.features[3].center)
-			.setPopup(popUp)
+			.setLngLat(results.features[0].center)
 			.addTo(map)
-			.togglePopup()
-
 	})
 }
