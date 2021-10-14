a1667339c02acc3bd2e10257086ad9e8da3853a2
Bosch Leith <bosch.leith@gmail.com>
2021-10-14 16:52:41 -0500

    Completed fetch promises and chaining promises examples.

--- js/promises-lec.js
+++ js/promises-lec.js
@@ -1,21 +1,26 @@
-
 "use strict";
 
 /*********************************************
  *        USING FETCH WITH PROMISES
  ******************************************** */
 
-var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon');
+// var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon');
 // return of fetch request is a Promise object
 
 //TODO: console log the results of the above Promise. What is the result?
 
+// console.log(pokemonAPI);
 //TODO: add a method that runs if the Promise resolves successfully
-
 //TODO: add a method that runs if the Promise fails
-
 //TODO: In the callback function of the .then method, parse the response into JSON
 
+// pokemonAPI.then(function (results){
+// 	console.log(results);
+// 	// results.text().then((text)=>{console.log(text)});
+// 	results.json().then((resultsObj)=>console.log(resultsObj));
+// }).catch(function (error){
+// 	console.log(error);
+// })
 
 /*********************************************
  *              CHAINING PROMISES
@@ -24,6 +29,22 @@ var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon');
 // TODO: Using Promise chaining, add another .then method that returns the results property of
 //  the first returned response.
 //    Taking a look at this second return, what are the results? What is the shape of our data?
+
+// fetch('https://pokeapi.co/api/v2/pokemon')
+// 	.then(function (results){
+// 	console.log(results);
+// 	// results.text().then((text)=>{console.log(text)});
+// 	results.json()
+// 		.then((resultsObj)=>{return resultsObj.result})
+// 	.then((pokemon)=>console.log(pokemon[0]));
+// }).catch(function (error){
+// 	console.log(error);
+// })
+
+fetch('https://pokeapi.co/api/v2/pokemon')
+	.then((response)=>response.json())
+	.then((jsonData)=>jsonData.results)
+	.then((results)=>results.forEach((result)=>console.log(result.name)));
 
 // TODO: Finally, chain another .then method that would log all of the name properties of the
 //  returned pokemon.
